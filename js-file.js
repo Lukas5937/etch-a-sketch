91dda16cd2c5da6af56f46fb6f5759facf4172b0
Lukas5937 <lukas-reich@t-online.de>
2023-09-05 12:33:11 +0200

    Add button to change the size of the grid

--- js-file.js
+++ js-file.js
@@ -1,9 +1,38 @@
 const container = document.querySelector(".container");
+const size = document.querySelector(".size");
+let sizeFactor = 16;
+let cellSize = (960 / sizeFactor) +"px";
+let cellAmount = sizeFactor ** 2;
 
-for (let i = 1; i <= 256; i++) {
-    let cell = document.createElement("div");
-    cell.classList.add("cell");
-    container.appendChild(cell);
-    cell.addEventListener("mouseover", () => {
-        cell.classList.add("hover");})
+function createGrid () {
+    for (let i = 1; i <= cellAmount; i++) {
+        let cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.setAttribute("id", i);
+        cell.style.height = cellSize;
+        cell.style.width = cellSize;
+        container.appendChild(cell);
+        cell.addEventListener("mouseover", () => {
+            cell.classList.add("hover");});
+    };
 }
+
+function removeGrid () {
+    for (let i = 1; i <= cellAmount; i++) {
+        cell = document.getElementById(i);
+        container.removeChild(cell);
+    };
+};
+
+let changeSize = () => {
+    sizeFactor = prompt("How many squares per site do you want?");
+    cellSize = (960 / sizeFactor) +"px";
+    removeGrid();
+    cellAmount = sizeFactor ** 2;
+    createGrid();
+};
+
+size.addEventListener("click", changeSize);
+
+createGrid();
+
